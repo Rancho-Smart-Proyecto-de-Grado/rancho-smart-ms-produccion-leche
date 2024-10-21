2bf697daa27728954c41769e44534f17d31416b7
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-21 03:31:34 -0500

    :wrench: chore: Configuración con Docker y Docker Compose

--- src/main/java/com/rancho_smart/ms_produccion_leche/MsProduccionLecheApplication.java
+++ src/main/java/com/rancho_smart/ms_produccion_leche/MsProduccionLecheApplication.java
@@ -2,8 +2,10 @@ package com.rancho_smart.ms_produccion_leche;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsProduccionLecheApplication {
 
 	public static void main(String[] args) {
